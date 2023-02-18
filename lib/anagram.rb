d65b14d6180d10fc17cef18bd9dafc963467b26a
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-02-18 12:54:10 +0300

    complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+
+    def match(possible_anagrams)
+      possible_anagrams.select do |possible_anagram|
+        possible_anagram.downcase.chars.sort == @word.downcase.chars.sort && possible_anagram.downcase != @word.downcase
+      end
+    end
+  end
